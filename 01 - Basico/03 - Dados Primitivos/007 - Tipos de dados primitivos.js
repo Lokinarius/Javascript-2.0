594e40bcef1c54d69dc82db25a813d70ab3783c4
William Galvão <Liam_draven@outlook.com>
2023-12-11 17:29:45 -0300

    Tipos primitivos

---
+++ 01 - Basico/03 - Dados Primitivos/007 - Tipos de dados primitivos.js
@@ -0,0 +1,17 @@
+const nome = 'Will'; //string
+const nome1 = "Liam"; //string
+const nome2 = `William`; //template string
+const num1 = 10; //number
+const num2 = 10.55; //number
+let nomeSobrenome; //undefined -> não aponta para local nenhum na memória
+const numeroFila = null; // Nulo -> não aponta para nenhum local na memória
+const aprovado = true; //boolean = true ou false(lógico)
+const reprovado = false; //boolean
+
+//o comando 'console.log(typeof)' serve para identificar qual tipo é a variável
+console.log(typeof nome)
+console.log(typeof num1)
+console.log(typeof nomeSobrenome)
+console.log(typeof numeroFila) //typeof identifica o "null" como um "object"
+console.log(typeof aprovado)
+
