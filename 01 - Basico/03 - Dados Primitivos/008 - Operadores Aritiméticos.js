8ff6dbfe7a7c97e28d7510eadc86f9ce99f53664
William Galvão <Liam_draven@outlook.com>
2023-12-11 17:29:59 -0300

    Operadores

---
+++ 01 - Basico/03 - Dados Primitivos/008 - Operadores Aritiméticos.js
@@ -0,0 +1,51 @@
+/*
+Aritiméticos
++ -> Adição e Contenação
+- -> Subtração
+* -> Mutiplicação
+/ -> Divisão
+** - > Potenciação
+% -> Resto da divisão
+*/
+
+// ++ adiciona valor de 1 a variavel
+let contador = 2;
+contador++;
+console.log(contador);
+// -- diminui valor de 1 a variavel
+let subtrator = 2;
+subtrator--;
+console.log(subtrator);
+/*
+Incremento +=
+Decremento -=
+*/
+let cont = 2
+cont += 2
+cont += 2
+cont += 2
+cont += 2
+console.log(cont)
+
+let desc = 30
+desc -= 3
+desc -= 3
+desc -= 3
+desc -= 3
+desc -= 3
+console.log(desc)
+
+let mult = 4
+mult *= 2
+mult *= 3
+mult *= 4
+console.log(mult)
+
+let pot = 3
+pot **= 5
+console.log(pot)
+
+/*
+parseInt --> Converte uma string em um número inteiro
+parseFloat --> Converte uma string em um número flutuante
+*/
