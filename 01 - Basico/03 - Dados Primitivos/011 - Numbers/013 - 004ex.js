bd3ff1112a2ad306f1345eaa3894afeef417d40b
William Galvão <Liam_draven@outlook.com>
2024-01-22 23:56:24 -0300

    2024

--- 01 - Basico/03 - Dados Primitivos/011 - Numbers/013 - 004ex.js
+++ 01 - Basico/03 - Dados Primitivos/011 - Numbers/013 - 004ex.js
@@ -1,4 +1,6 @@
-const num = prompt("Digite seu número:");
+const numStr = prompt("Digite seu número:");
+const num = parseFloat(numStr);
+
 document.body.innerHTML += `<h2><strong>Seu número é ${num}</strong></h2></br>`;
 document.body.innerHTML += `A raiz quadrada: ${num ** 0.5}</br>`;
 document.body.innerHTML += `${num} é inteiro:${Number.isInteger(num)}</br>`;
