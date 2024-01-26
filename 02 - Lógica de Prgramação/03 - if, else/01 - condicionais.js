03935b19122f11e01a22229b5dff916e6dc7cb43
William Galvão <Liam_draven@outlook.com>
2024-01-26 16:44:39 -0300

    condicionais

---
+++ 02 - Lógica de Prgramação/03 - if, else/01 - condicionais.js
@@ -0,0 +1,15 @@
+const numero = 10;
+if(numero >= 0 && numero <= 5){
+    console.log('seu número está entre 0 e 5');
+} else{
+    console.log('seu número não está entre 0 e 5');
+}
+
+const hora = 3;
+if( hora >= 4 && hora < 12){
+    console.log('Bom dia');
+} else if (hora >= 12 && hora <= 18){
+    console.log('Boa tarde');
+} else{
+    console.log('Boa noite');
+}
