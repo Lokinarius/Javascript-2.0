59c238a70f6a9239c11c02fd198db42d6bd8ad2a
William Galvão <Liam_draven@outlook.com>
2024-02-15 22:22:04 -0300

    date

---
+++ 02 - Lógica de Prgramação/06 - Objeto date/script.js
@@ -0,0 +1,9 @@
+const data = new Date();
+//new = função construtora
+console.log('Dia', data.getDate());
+console.log('Mês', data.getMonth() + 1); //Mês começa no zero
+console.log('Ano', data.getFullYear());
+console.log('Hora', data.getHours());
+console.log('Min', data.getMinutes());
+console.log('Seg', data.getSeconds());
+console.log(data.toString());
